a0e931eeca49ab80cb8c4c82d7aeea7ee1ffe633
Marine <marine.kazemi@gmail.com>
2022-10-25 16:04:48 +0200

    Update JeuVideoEtFilm.py

--- SemaineOctobre/JeuVideoEtFilm.py
+++ SemaineOctobre/JeuVideoEtFilm.py
@@ -11,7 +11,6 @@ import pandas as pad
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# In[6]:
 st.set_page_config(page_title="Jeu Vidéo et Film", page_icon=":tada:", layout="wide")
 
 st.subheader("Python et l'analyse de données :computer:")
@@ -20,7 +19,6 @@ st.write("Le monde du jeu vidéo était très connu à l'époque pour ses adapta
 st.write("Nous pouvons par exemple prendre la fameuse boîte française 'Infogrames' qui fut fortement moquée par des vidéastes dédiés au jeux vidéos pour ses jeux provenant de film de très mauvaises qualités.")
 st.write("Toutefois, l'inverse existait aussi ! Les jeux vidéos étant une source abondante d'idée, certains producteurs ont décidés de les développer en film.")
 st.write("Mais est-ce qu'un jeu vidéo qui a très bien fonctionner va aussi fonctionner dans le monde du cinéma ? Ceci est notre problèmatique pour cette analyse de donnée.")
-# In[5]:
 
 
 video_game_film = pad.read_csv('video_game_films.csv')
@@ -28,14 +26,10 @@ video_game = pad.read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv')
 
 st.write("Pour notre analyse de données, nous allons utiliser deux csv, l'un regroupe les jeux vidéos adaptés en film, tandis que l'autre la vente des jeux vidéos jusqu'en 2016.")
 
-# In[6]:
 
 st.dataframe(video_game_film)  # Same as st.write(df)
 
 video_game_film.head()
-
-
-# In[7]:
 
 
 video_game.head()
@@ -61,8 +55,6 @@ st.write("Avec cette technique il n'était plus nécéssaire de modifier le nom 
 st.title("Worldwide box office")
 
 st.write("Worldwide box office et Global_Sales sont des valeurs que nous allons utiliser plus tard car les deux sont des valeurs à peu pret comparable (se trouvant tous les deux en million). Le petit soucis est que l'un affiche toutes la somme tandis que l'autre s'affiche en 0.00. Pour régler ce problème, il fallait juste changer le format de l'un des deux csv (celui avec le moins de valeurs fut choisi pour le changement).")
-
-# In[8]:
 
 st.title("Importation 2")
 
@@ -91,9 +83,6 @@ st.dataframe(melange_f)
 st.write("Nous obtenons donc un jeu de données sur l'univers des jeux vidéos ayant eu une adaptation au cinéma avec 1172 jeux repartient dans moins de 42 franchises avec 24 caractéristiques.")
 st.write(" On peut déjà regarder sur un historigramme")
 
-
-# In[13]:
-
 fig = plt.figure(figsize=(15,8))
 
 sns.histplot(
@@ -104,8 +93,6 @@ sns.histplot(
 )
 plt.title("Les genre les plus crées dans les jeux vidéos")
 st.pyplot(fig)
-
-# In[14]:
 
 #sns.lineplot(data=melange_f, y='Global_Sales',x="Year_of_Release")
 st.info('Appuyez sur le bouton pour retirer les valeurs incohérentes', icon="ℹ️")
@@ -131,8 +118,6 @@ else:
     plt.title("Vente des jeux vidéos par année")
     st.pyplot(fig2) 
 
- 
-# In[35]:
 fig3 = plt.figure(figsize=(15,8))
 fig33 = plt.xticks(rotation=90)
 
@@ -145,8 +130,6 @@ sns.lineplot(
 plt.title("Graphique échantillon box office des films")
 st.pyplot(fig3,fig33)
 
-
-# In[20]:
 
 st.info('Appuyez sur le bouton pour retirer les valeurs incohérentes', icon="ℹ️")
 
@@ -175,8 +158,6 @@ else:
     st.pyplot(fig4,fig44)
 
 
-# In[22]:
-
 
 fig5 = plt.figure(figsize=(15,8))
 fig55 = plt.xticks(rotation=90)
@@ -189,9 +170,6 @@ sns.barplot(
 )
 plt.title("Graphique des genres les plus vendu au cinéma")
 st.pyplot(fig5,fig55)
-
-
-# In[23]:
 
 
 fig6 = plt.figure(figsize=(15,8))
@@ -214,15 +192,11 @@ st.title("TEST AVEC DES JEUX VIDEOX / FILMS INDIVIDUELLEMENTS")
 # ##### Resident Evil
 
 st.title("RESIDENT EVIL :zombie:")
-# In[27]:
-
 
 RE = melange_f[melange_f['Surnom']=="Resident Evil"]
 RE = RE.reset_index(drop=True)
 RE
 
-
-# In[28]:
 
 fig7 = plt.figure(figsize=(15,8))
 
@@ -234,8 +208,6 @@ sns.boxplot(
 )
 st.pyplot(fig7)
 
-# In[36]:
-
 fig8 = plt.figure(figsize=(15,8))
 sns.boxplot(
     data=RE,
@@ -244,8 +216,6 @@ sns.boxplot(
 
 )
 st.pyplot(fig8)
-
-# In[41]:
 
 
 fig9 = plt.figure(figsize=(15,8))
@@ -259,7 +229,6 @@ st.pyplot(fig9)
 
 # ##### Lara Croft
 
-# In[31]:
 
 st.title("LARA CROFT :woman:")
 
@@ -268,7 +237,6 @@ LC = LC.reset_index(drop=True)
 LC
 
 
-# In[37]:
 fig10 = plt.figure(figsize=(15,8))
 sns.barplot(
     data=LC,
@@ -279,8 +247,6 @@ sns.barplot(
 st.pyplot(fig10)
 
 
-# In[39]:
-
 fig11 = plt.figure(figsize=(15,8))
 sns.barplot(
     data=LC,
@@ -290,7 +256,6 @@ sns.barplot(
 )
 st.pyplot(fig11)
 
-# In[37]:
 fig12 = plt.figure(figsize=(15,8))
 sns.barplot(
     data=LC,
@@ -303,7 +268,6 @@ st.pyplot(fig12)
 
 # #### Alone in the Dark
 
-# In[42]:
 
 st.title("Alone in the dark :ghost:")
 AID = melange_f[melange_f['Surnom']=="Alone in the Dark"]
@@ -311,7 +275,6 @@ AID = AID.reset_index(drop=True)
 AID
 
 
-# In[44]:
 
 fig11 = plt.figure(figsize=(15,8))
 sns.barplot(
@@ -323,7 +286,6 @@ sns.barplot(
 st.pyplot(fig11)
 
 
-# In[52]:
 
 st.write("Worldwide box office pour Alone in the Dark : ", AID['Worldwide_box_office_tri'][0])
 
@@ -333,17 +295,3 @@ st.write("Worldwide box office pour Alone in the Dark : ", AID['Worldwide_box_of
 # #### Conclusion
 st.title("Conclusion")
 st.write("Nous pouvons conclure que même si le film dont vient le jeux vidéo peut influencer la réussite du film, cela n'est pas toujours le cas. Il faut aussi choisir des meilleurs données car les csv n'étaient pas si compatible que ça.")
-
-# Nous pouvons conclure que même si le film dont vient le jeux vidéo peut influencer la réussite du film, cela n'est pas toujours le cas. Il faut aussi choisir des meilleurs données car les csv n'étaient pas si compatible que ça.
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
