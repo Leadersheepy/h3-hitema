4349d94fa4b723425c26d1086f513b64d4663c00
Marine <marine.kazemi@gmail.com>
2022-10-25 15:54:13 +0200

    Update streamlit.py

--- SemaineOctobre/streamlit.py
+++ SemaineOctobre/streamlit.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import streamlit as st
+import numpy as np
+import pandas as pad
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Jeu Vidéo et Film", page_icon=":tada:", layout="wide")
 
@@ -8,3 +13,6 @@ st.write("Le monde du jeu vidéo était très connu à l'époque pour ses adapta
 st.write("Nous pouvons par exemple prendre la fameuse boîte française 'Infogrames' qui fut fortement moquée par des vidéastes dédiés au jeux vidéos pour ses jeux provenant de film de très mauvaises qualités.")
 st.write("Toutefois, l'inverse existait aussi ! Les jeux vidéos étant une source abondante d'idée, certains producteurs ont décidés de les développer en film.")
 st.write("Mais est-ce qu'un jeu vidéo qui a très bien fonctionner va aussi fonctionner dans le monde du cinéma ? Ceci est notre problèmatique pour cette analyse de donnée.")
+
+
+video_game_film = pad.read_csv('video_game_films.csv')
