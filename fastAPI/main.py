7c76a5c5ecec0b53419030434b9781764e3ecd7a
Marine <marine.kazemi@gmail.com>
2022-10-27 10:40:15 +0200

    fastapi

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/component/{component_id}") #path parameter
+async def get_component(component_id: int):
+    return {"component_id" : component_id}
+ 
+@app.get("/component/")
+async def read_component(number: int, text: str):
+    return {"number" : number, "text": text}
+
+#http://127.0.0.1:8000/component/?number=6&text=camarche
+
+#if __name__ == "__main__":
+#   uvicorn.run(app, host="127.0.0.1", port=8000)
