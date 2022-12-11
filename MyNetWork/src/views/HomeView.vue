240849c8f3fa51fd2791a67904fcc20aae6735ef
Marinator89 <43178189+Leadersheepy@users.noreply.github.com>
2022-12-11 18:48:23 +0100

    Suite

--- MyNetWork/src/views/HomeView.vue
+++ MyNetWork/src/views/HomeView.vue
@@ -12,9 +12,9 @@
         <p class="status">Tesla drivers just passed 3 billion electric miles, saving the world 120M gallons of gas 👍</p>
         <img class="img-content" src="https://via.placeholder.com/350x150" />
         
-        <div class="action">
+        <div class="action headerPost">
 
-            <div class="comment">
+            <div class="commentary">
                 <a href="#">
                     <img src="../assets/comment.png" />
                     <p>Counter</p>
@@ -29,6 +29,54 @@
             </div>
             
         </div>
-    </div>
 
-</template>
+        <h3 class="commentaire"> Commentaires : </h3>
+        
+        <div class="comment">
+            <div class="comment-author">
+                <h3 class="comment-name">John Doe</h3>
+                <p class="comment-date">December 11, 2022</p>
+            </div>
+            <p class="comment-content">This is a comment on the post.</p>
+        </div>
+    </div>
+    
+    </template>
+
+
+<style>
+  /* Style the comment using CSS */
+  .comment {
+    margin-top: 20px;
+    border: 1px solid rgb(207, 204, 204);
+    border-radius: 10px;
+  }
+
+  .comment-author {
+    display: flex;
+    align-items: center;
+  }
+
+  .comment-avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .comment-name {
+    font-size: 18px;
+    margin-left: 10px;
+  }
+
+  .comment-date {
+    font-size: 14px;
+    color: #999;
+    margin-left: 10px;
+  }
+
+  .comment-content {
+    font-size: 16px;
+    line-height: 1.5;
+    margin-top: 10px;
+  }
+</style>
